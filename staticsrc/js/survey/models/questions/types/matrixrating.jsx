a288b3826f92857eeeb357ea17bb5194ece4b547
Alexei White <white@Alexeis-MacBook-Pro.local>
2017-07-30 16:55:46 -0700

    rating widget

--- staticsrc/js/survey/models/questions/types/matrixrating.jsx
+++ staticsrc/js/survey/models/questions/types/matrixrating.jsx
@@ -29,22 +29,61 @@ class MatrixRatingQuestion extends React.Component {
  * Render the view
  */
   render() {
+    let qname = this.props.name;
+    let ratingScale = [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7
+    ];
+    let ctx = this;
+    let answer = 0;
     return (
       <div className="question--matrixrating">
         <div className="matrix--carousel">
           {this
-          .props
-          .choices
-          .map((rt, idx) => {
-            return <div
-              key={idx}
-              className={"choiceitem"}><div className="choiceinner">{rt}</div></div>
-          })}
-          <a href="#" className="carousel--control left fa fa-chevron-left" title="Previous"></a>
-          <a href="#" className="carousel--control right fa fa-chevron-right" title="Next"></a>
+            .props
+            .choices
+            .map((rt, idx) => {
+              return <div key={idx} className={"choiceitem"}>
+                <div className="choiceinner">{rt}</div>
+              </div>
+            })}
+          <a
+            href="#"
+            className="carousel--control left fa fa-chevron-left"
+            title="Previous"></a>
+          <a
+            href="#"
+            className="carousel--control right fa fa-chevron-right"
+            title="Next"></a>
         </div>
         <div className="rating-zone">
-          rater
+          <div className="question--rating">
+            <div className="buttongroup">
+              {ratingScale.map((rt, idx) => {
+                return <label
+                  key={idx}
+                  className={"selectbutton question--ratingitem" + (!!(rt <= answer)
+                  ? " selected"
+                  : "")}>{rt}<input
+                  type="checkbox"
+                  checked={!!(rt <= answer)}
+                  name={qname}
+                  value={rt}
+                  onChange={ctx
+                  .handleAnswerChange
+                  .bind(ctx)}/></label>
+              })}
+            </div>
+            <div className="labelcontainer">
+              {this.props.low && <span className="smalllabel lowlabel">{this.props.low}</span>}
+              {this.props.high && <span className="smalllabel highlabel">{this.props.high}</span>}
+            </div>
+          </div>
         </div>
       </div>
     );
