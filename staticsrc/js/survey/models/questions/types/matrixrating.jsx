2347547c8474bc036e3c940143ad835bf8d11d92
Alexei White <white@Alexeis-MacBook-Pro.local>
2017-07-31 22:28:16 -0700

    Matrix forward back

--- staticsrc/js/survey/models/questions/types/matrixrating.jsx
+++ staticsrc/js/survey/models/questions/types/matrixrating.jsx
@@ -12,6 +12,11 @@ class MatrixRatingQuestion extends React.Component {
  */
   constructor(props) {
     super(props);
+    this.state = {
+      selectedItem: 0,
+      animatingForward: false,
+      animatingBackward: false
+    };
   }
 
   /**
@@ -19,10 +24,63 @@ class MatrixRatingQuestion extends React.Component {
    * @param {*} e
    */
   handleAnswerChange(e) {
-    /*let targ = e && e.target,
+    /* let targ = e && e.target,
       root = ReactDOM.findDOMNode(this),
       ipts = root.getElementsByClassName("main--textfield");
     this.props.dispatch(changeAnswer(this.props.name, ipts[0].value));*/
+  }
+
+  /**
+   * Go back
+   * @param {*} e
+   */
+  retreatCarousel(e) {
+    if (e) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+    if (this.state.selectedItem > 0 && !this.state.animatingBackward && !this.state.animatingForward) {
+      // do it
+      this.setState({animatingBackward: true, animatingForward: false});
+    }
+  }
+
+  /**
+   * Advance
+   * @param {*} e
+   */
+  advanceCarousel(e) {
+    if (e) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+    if (this.state.selectedItem < (this.props.choices.length - 1) && !this.state.animatingBackward && !this.state.animatingForward) {
+      // do it
+      this.setState({animatingBackward: false, animatingForward: true});
+    }
+  }
+
+  /**
+   * Animation ended
+   * @param {*} e
+   */
+  handleAnimationEnd(e) {
+    let targ = e.target;
+    if (targ.className.indexOf('choiceitem') > -1) {
+      if (this.state.animatingForward) {
+        this.setState({
+          animatingBackward: false,
+          animatingForward: false,
+          selectedItem: this.state.selectedItem + 1
+        });
+      } else if (this.state.animatingBackward) {
+        this.setState({
+          animatingBackward: false,
+          animatingForward: false,
+          selectedItem: this.state.selectedItem - 1
+        });
+      }
+    }
   }
 
   /**
@@ -39,27 +97,62 @@ class MatrixRatingQuestion extends React.Component {
       6,
       7
     ];
-    let ctx = this;
-    let answer = 0;
+    let ctx = this,
+      selectedItem = this.state.selectedItem,
+      animatingBackward = this.state.animatingBackward,
+      animatingForward = this.state.animatingForward,
+      answer = 0;
     return (
-      <div className="question--matrixrating">
+      <div
+        className="question--matrixrating"
+        onTransitionEnd={this
+        .handleAnimationEnd
+        .bind(this)}>
         <div className="matrix--carousel">
           {this
             .props
             .choices
             .map((rt, idx) => {
-              return <div key={idx} className={"choiceitem"}>
+              return <div
+                key={idx}
+                className={"choiceitem" + (selectedItem == idx
+                ? " selected"
+                : "") + (selectedItem == (idx + 1)
+                ? " prevselected"
+                : "") + (selectedItem == (idx - 1)
+                ? " nextselected"
+                : "") + ((selectedItem == (idx + 1) && animatingBackward)
+                ? " abPrevSelected"
+                : "") + ((selectedItem == idx && animatingForward)
+                ? " afSelected"
+                : "") + ((selectedItem == idx && animatingBackward)
+                ? " abSelected"
+                : "") + ((selectedItem == (idx - 1) && animatingForward)
+                ? " afNextSelected"
+                : "") + ((selectedItem == (idx + 1) && animatingBackward)
+                ? " afPrevSelected"
+                : "")}>
                 <div className="choiceinner">{rt}</div>
               </div>
             })}
           <a
             href="#"
-            className="carousel--control left fa fa-chevron-left"
-            title="Previous"></a>
+            className={"carousel--control left fa fa-chevron-left" + (selectedItem > 0
+            ? " visible"
+            : "")}
+            title="Previous"
+            onClick={this
+            .retreatCarousel
+            .bind(this)}></a>
           <a
             href="#"
-            className="carousel--control right fa fa-chevron-right"
-            title="Next"></a>
+            className={"carousel--control right fa fa-chevron-right" + (selectedItem < (this.props.choices.length - 1)
+            ? " visible"
+            : "")}
+            title="Next"
+            onClick={this
+            .advanceCarousel
+            .bind(this)}></a>
         </div>
         <div className="rating-zone">
           <div className="question--rating">
