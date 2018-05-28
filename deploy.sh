b6856af5087eb42393b0f9a7fe2e833355591a1b
Alexei White <white@Alexeis-MacBook-Pro.local>
2018-05-28 12:15:09 -0700

    Survey is deploying now

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 npm run prod
 rm dist/survey.zip
 rm package-lock.json
-zip -r dist/survey.zip . -x ".*" -x "*.DS_Store" -x "sessions*" -x "staticsrc*" -x "node_modules*" -x "*.zip" -x "build*" -x "npm*" -x "dump.rdb" -x "*.sh" -x "dev*" -x "package-lock*"
+zip -r dist/survey.zip . -x ".git/*" -x "*.DS_Store" -x "sessions*" -x "staticsrc*" -x "node_modules*" -x "*.zip" -x "build*" -x "npm*" -x "dump.rdb" -x "*.sh" -x "dev*" -x "package-lock*"
