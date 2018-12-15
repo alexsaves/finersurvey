dbb6101ebba4c252247cbd4b2a280ef040fca7c9
Alexei White <alexei.white@gmail.com>
2018-12-14 21:47:54 -0800

    0.5.1

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-npm run prod
-rm dist/survey.zip
-rm package-lock.json
-zip -r dist/survey.zip . -x ".git/*" -x "*.DS_Store" -x "sessions*" -x "staticsrc*" -x "node_modules*" -x "*.zip" -x "build*" -x "npm*" -x "dump.rdb" -x "*.sh" -x "dev*" -x "package-lock*"
